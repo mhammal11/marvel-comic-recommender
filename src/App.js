253373537d8db51c8efd4a88f794beba887437f8
Michael Hammal <hammalm@mcmaster.ca>
2023-12-21 20:07:32 -0500

    Create basic React skeleton and website header

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// Example usage in src/App.js
+import React from 'react';
+import Header from './components/Header/Header';
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+            {/* Other components */}
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+// src/components/Header/Header.js
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header className="app-header">
+            <h1>Marvel Comics Recommender</h1>
+            {/* You can add navigation or other information here */}
+        </header>
+    );
+};
+
+export default Header;
