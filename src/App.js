ee4e0bb7163c0ec480687c9ca18bc7b54a8ebb51
Michael Hammal <hammalm@mcmaster.ca>
2023-12-25 20:15:58 -0500

    Implement the home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // Example usage in src/App.js
 import React from 'react';
 import Header from './components/Header/Header';
+import Home from './pages/Home';
 
 const App = () => {
     return (
         <div>
             <Header />
+            <Home />
             {/* Other components */}
         </div>
     );
